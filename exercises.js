ecc2cc006a7910c422f65206f5b62c783b2d8eba
bilal05476 <ba4762313@gmail.com>
2022-10-02 16:55:57 +0500

    updates

--- exercises.js
+++ exercises.js
@@ -414,3 +414,95 @@
 //     document.write(`${item} username is available! <br />`);
 //   }
 // });
+
+// Exercise 33
+// var num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// num.map((item) => {
+//   if (item === 1) {
+//     document.write(`${item}st <br />`);
+//   } else if (item === 2) {
+//     document.write(`${item}nd <br />`);
+//   } else if (item === 3) {
+//     document.write(`${item}rd <br />`);
+//   } else {
+//     document.write(`${item}th <br />`);
+//   }
+// });
+
+// Exercise 34
+// var fav_pizza = ["Arabic Ranch", "Delight Fajita", "BBQ Chicken Cheese"];
+// for (var i = 0; i < fav_pizza.length; i++) {
+//   document.write(`I like ${fav_pizza[i]} pizza. <br />`);
+// }
+// document.write(
+//   "I like these kinds of pizza. I love these kinds of pizza. I would love to eat these kinds of pizza!"
+// );
+
+// Exercise 35
+// var animnals = ["Lion", "Tiger", "Cheetah"];
+// for (var i = 0; i < animnals.length; i++) {
+//   document.write(`${animnals[i]} have the good smell ability. <br />`);
+// }
+// document.write(
+//   "Any of these animals could have be dangerous for humans. <br />"
+// );
+
+// Exercise 36
+// function make_shirt(size, text) {
+//   document.write(
+//     `The size of shirt should be: ${size}. <br />The message printed on the shirt should be: ${text}. <br />`
+//   );
+// }
+// make_shirt("small", "Hello world from JavaScript");
+
+// Exercise 37
+// function make_shirt(size, text) {
+//   if (size === "large" || size === "medium") {
+//     document.write("I Love JavaScript");
+//   } else {
+//     document.write(
+//       `${text}. <br />`
+//     );
+//   }
+// }
+// make_shirt("small", "Hello world from JavaScript");
+
+// Exercise 38
+// function describe_city(city, country = "Pakistan") {
+//   document.write(`${city} is in ${country} <br />`);
+// }
+// describe_city("karachi");
+// describe_city("lahore");
+// describe_city("Baku", "Azerbaijan");
+
+// Exercise 39
+// function city_country(city, country) {
+//   return `"${city}, ${country}" <br />`;
+// }
+// document.write(city_country("Karachi", "Pakistan"));
+// document.write(city_country("Baku", "Azerbaijan"));
+// document.write(city_country("Sharjah", "UAE"));
+
+// Exercise 40
+// function make_album(artistName, albumTitle, numTrack) {
+//   const musicAlbum = {
+//     artistName,
+//     albumTitle,
+//     numTrack,
+//   };
+//   if (numTrack) {
+//     return `Artist: ${musicAlbum.artistName}, Album: ${musicAlbum.albumTitle}, Num of Track: ${musicAlbum.numTrack} <br />`;
+//   } else {
+//     return `Artist: ${musicAlbum.artistName}, Album: ${musicAlbum.albumTitle} <br />`;
+//   }
+// }
+// document.write(make_album("Atif Aslam", "Aarzoo"));
+// document.write(make_album("Arijit Singh", "Ashiqui"));
+// document.write(make_album("Rahat Fateh Ali", "Zindagi", 21));
+
+// Exercise 41
+// var magicians = ["kallie", "brownie", "Loodi"];
+// function show_magicians(magician) {
+//   return `${magician} <br />`;
+// }
+// magicians.map((item) => document.write(show_magicians(item)));
